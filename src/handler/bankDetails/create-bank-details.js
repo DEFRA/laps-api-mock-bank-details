15c9e492ac5a0f9ad165e78c8722e5a24846db6b
Dennis <dennis.effa.amponsah@accenture.com>
2025-10-14 16:13:12 +0100

    adds create bank details endpoint

---
+++ src/handler/bankDetails/create-bank-details.js
@@ -0,0 +1,20 @@
+import Boom from '@hapi/boom'
+
+const createBankDetails = (request, h) => {
+  const payload = request.payload
+  if (!payload) {
+    throw Boom.badRequest('No payload provided')
+  }
+
+  return h.response({
+    id: '12345-abcde-67890-fghij',
+    accountNumber: '094785923',
+    accountName: 'Defra Test',
+    sortCode: '09-03-023',
+    confirmed: false,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  })
+}
+
+export { createBankDetails }
