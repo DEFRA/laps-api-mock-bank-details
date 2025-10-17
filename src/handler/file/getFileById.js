c4126c606b016bfcfed78e43666db4b255c0a2e7
Dennis <dennis.effa.amponsah@accenture.com>
2025-10-17 15:41:29 +0100

    updates endpoint to get file by id

--- src/handler/file/getFileById.js
+++ src/handler/file/getFileById.js
@@ -1,14 +1,20 @@
 import Boom from '@hapi/boom'
+import { documents } from './data.js'
 
 const getFileById = (request, h) => {
   const localAuthority = request.params.id
-
   if (localAuthority === 'invalid') throw Boom.badRequest('Invalid ID')
+  const documentName = getFileNameById(localAuthority)
 
-  return h.file('./src/handler/file/file_report_2024.pdf', {
+  return h.file(`./src/handler/file/${documentName}`, {
     mode: 'attachment', // forces download
-    filename: 'file_report_2024.pdf'
+    filename: documentName
   })
+}
+
+function getFileNameById(id) {
+  const doc = documents.find((document) => document.id === id)
+  return doc ? doc.fileName : null // Returns null if no document found
 }
 
 export { getFileById }
--- src/handler/file/getFilesListByLocalAuthority.js
+++ src/handler/file/getFilesListByLocalAuthority.js
@@ -1,82 +1,12 @@
 import Boom from '@hapi/boom'
+import { documents } from './data.js'
 
 const getFilesListByLocalAuthority = (request, h) => {
   const localAuthority = request.params.localAuthority
 
   if (localAuthority === 'invalid') throw Boom.badRequest('Invalid Authority')
 
-  return h.response([
-    {
-      id: '12345-abcde-67890-fghij',
-      fileName: 'grant-determination-letter.pdf',
-      localAuthority: 'Newcastle City Council',
-      financialYear: '2024',
-      quarter: 'Q1',
-      creationDate: '2024-06-15T00:00:00.000Z',
-      documentType: 'grant',
-      language: 'EN'
-    },
-    {
-      id: '123153-1111-67890-fghij',
-      fileName: 'notice-of-assessment.pdf',
-      localAuthority: 'Newcastle City Council',
-      financialYear: '2024',
-      quarter: 'Q2',
-      creationDate: '2024-08-27T00:00:00.000Z',
-      documentType: 'notice_of_assessment',
-      language: 'EN'
-    },
-    {
-      id: 'yyyaaa-12345-bbcd-67ef',
-      fileName: 'remittance-advice.pdf',
-      localAuthority: 'Newcastle City Council',
-      financialYear: '2025',
-      quarter: 'Q4',
-      creationDate: '2025-01-01T00:00:00.000Z',
-      documentType: 'remittance ',
-      language: 'EN'
-    },
-    {
-      id: '082h-abcde-67890-fghij',
-      fileName: 'grant-determination-letter.pdf',
-      localAuthority: 'Newcastle City Council',
-      financialYear: '2025',
-      quarter: 'Q1',
-      creationDate: '2024-05-20T00:00:00.000Z',
-      documentType: 'grant',
-      language: 'EN'
-    },
-    {
-      id: '123153-1111-67890-fghij',
-      fileName: 'notice-of-assessment.pdf',
-      localAuthority: 'Newcastle City Council',
-      financialYear: '2025',
-      quarter: 'Q2',
-      creationDate: '2024-07-27T00:00:00.000Z',
-      documentType: 'notice_of_assessment',
-      language: 'EN'
-    },
-    {
-      id: 'yyyaaa-12345-bbcd-67ef',
-      fileName: 'remittance-advice.pdf',
-      localAuthority: 'Newcastle City Council',
-      financialYear: '2025',
-      quarter: 'Q3',
-      creationDate: '2025-11-01T00:00:00.000Z',
-      documentType: 'remittance ',
-      language: 'EN'
-    },
-    {
-      id: 'sdfss-12345-bbcd-67ef',
-      fileName: 'remittance-advice.pdf',
-      localAuthority: 'Newcastle City Council',
-      financialYear: '2026',
-      quarter: 'Q4',
-      creationDate: '2026-02-01T00:00:00.000Z',
-      documentType: 'remittance ',
-      language: 'EN'
-    }
-  ])
+  return h.response(documents)
 }
 
 export { getFilesListByLocalAuthority }
