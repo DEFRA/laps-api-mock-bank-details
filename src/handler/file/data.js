3737d318c222559298e73f45e0c1418f58512da8
juhiupreti-acc <juhi.upreti@accenture.com>
2025-10-22 15:31:26 +0100

    Add welsh files

--- src/handler/file/data.js
+++ src/handler/file/data.js
@@ -68,5 +68,75 @@ export const documents = [
     creationDate: '2026-02-01T00:00:00.000Z',
     documentType: 'remittance',
     language: 'EN'
+  },
+  {
+    id: '5678-abcde-12390-fghij',
+    fileName: 'grant-determination-letter_cy.pdf',
+    localAuthority: 'Newcastle City Council',
+    financialYear: '2024',
+    quarter: 'Q1',
+    creationDate: '2024-06-15T00:00:00.000Z',
+    documentType: 'grant',
+    language: 'CY'
+  },
+  {
+    id: '2222-1111-67890-fghij',
+    fileName: 'notice-of-assessment_cy.pdf',
+    localAuthority: 'Newcastle City Council',
+    financialYear: '2024',
+    quarter: 'Q2',
+    creationDate: '2024-08-27T00:00:00.000Z',
+    documentType: 'notice_of_assessment',
+    language: 'CY'
+  },
+  {
+    id: '3333-12345-bbcd-67ef',
+    fileName: 'remittance-advice_cy.pdf',
+    localAuthority: 'Newcastle City Council',
+    financialYear: '2025',
+    quarter: 'Q4',
+    creationDate: '2025-01-01T00:00:00.000Z',
+    documentType: 'remittance',
+    language: 'CY'
+  },
+  {
+    id: '4444-abcde-67890-fghij',
+    fileName: 'grant-determination-letter_cy.pdf',
+    localAuthority: 'Newcastle City Council',
+    financialYear: '2025',
+    quarter: 'Q1',
+    creationDate: '2024-05-20T00:00:00.000Z',
+    documentType: 'grant',
+    language: 'CY'
+  },
+  {
+    id: '55555-1111-67890-fghij',
+    fileName: 'notice-of-assessment_cy.pdf',
+    localAuthority: 'Newcastle City Council',
+    financialYear: '2025',
+    quarter: 'Q2',
+    creationDate: '2024-07-27T00:00:00.000Z',
+    documentType: 'notice_of_assessment',
+    language: 'CY'
+  },
+  {
+    id: '66666-12345-bbcd-67ef',
+    fileName: 'remittance-advice_cy.pdf',
+    localAuthority: 'Newcastle City Council',
+    financialYear: '2025',
+    quarter: 'Q3',
+    creationDate: '2025-11-01T00:00:00.000Z',
+    documentType: 'remittance',
+    language: 'CY'
+  },
+  {
+    id: '77777-12345-bbcd-67ef',
+    fileName: 'remittance-advice_cy.pdf',
+    localAuthority: 'Newcastle City Council',
+    financialYear: '2026',
+    quarter: 'Q4',
+    creationDate: '2026-02-01T00:00:00.000Z',
+    documentType: 'remittance',
+    language: 'CY'
   }
 ]
